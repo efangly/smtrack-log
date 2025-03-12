966cb9ba6c3a3fd0ac69144e856462a30abb6907
FangLy <thefangsni@gmail.com>
2025-03-12 22:05:20 +0700

    feat: validate log creation year and enhance Redis cache expiration

--- src/logday/logday.service.ts
+++ src/logday/logday.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateLogdayDto } from './dto/create-logday.dto';
 import { UpdateLogdayDto } from './dto/update-logday.dto';
 import { PrismaService } from '../prisma/prisma.service';
@@ -6,6 +6,7 @@ import { RedisService } from '../redis/redis.service';
 import { RabbitmqService } from '../rabbitmq/rabbitmq.service';
 import { dateFormat } from '../common/utils';
 import { DevicePayloadDto, JwtPayloadDto } from '../common/dto';
+import { format } from 'date-fns';
 
 @Injectable()
 export class LogdayService {
@@ -15,15 +16,19 @@ export class LogdayService {
     private readonly rabbitmq: RabbitmqService
   ) {}
   async create(createLogdayDto: CreateLogdayDto | CreateLogdayDto[], user: DevicePayloadDto) {
+    const currentYear = format(new Date(), "yyyy");
     if (Array.isArray(createLogdayDto)) {
       for (const log of createLogdayDto) {
-        log.serial = user.sn;
-        log.sendTime = dateFormat(log.sendTime);
-        this.rabbitmq.sendToLog<CreateLogdayDto>('logday', log);
-        this.rabbitmq.sendToDevice<CreateLogdayDto>('log-device', log);
+        if (format(log.sendTime, "yyyy") === currentYear) {
+          log.serial = user.sn;
+          log.sendTime = dateFormat(log.sendTime);
+          this.rabbitmq.sendToLog<CreateLogdayDto>('logday', log);
+          this.rabbitmq.sendToDevice<CreateLogdayDto>('log-device', log);
+        }
       }
       return createLogdayDto.length;
     } else {
+      if (format(createLogdayDto.sendTime, "yyyy") !== currentYear) throw new BadRequestException('Invalid year');
       createLogdayDto.serial = user.sn;
       createLogdayDto.sendTime = dateFormat(createLogdayDto.sendTime);
       this.rabbitmq.sendToLog<CreateLogdayDto>('logday', createLogdayDto);
@@ -40,7 +45,7 @@ export class LogdayService {
     const cache = await this.redis.get(`log:${id}`);
     if (cache) return JSON.parse(cache);
     const log = await this.prisma.logDays.findMany({ where: { serial: id } });
-    await this.redis.set(`log:${id}`, JSON.stringify(log), 10);
+    await this.redis.set(`log:${id}`, JSON.stringify(log), 15);
     return log;
   }
 
