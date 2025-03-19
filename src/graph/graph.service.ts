6679a82c37a7d2a962f8c5532c31fa38390bacdf
FangLy <thefangsni@gmail.com>
2025-03-19 14:31:13 +0700

    feat: enhance data aggregation in GraphService by adding aggregateWindow and expanding filter criteria

--- src/graph/graph.service.ts
+++ src/graph/graph.service.ts
@@ -17,22 +17,26 @@ export class GraphService {
         break;
       case 'week':
         query += '|> range(start: -1w) ';
+        query += '|> aggregateWindow(every: 10m, fn: first, createEmpty: false) ';
         break;
       case 'month':
         query += '|> range(start: -1mo) ';
+        query += '|> aggregateWindow(every: 30m, fn: first, createEmpty: false) ';
         break;
       default:
         if (!filter.includes(',')) throw new BadRequestException('Invalid filter');
         const date = filter.split(',');
         query += `|> range(start: ${date[0]}, stop: ${date[1]}) `;
+        query += '|> aggregateWindow(every: 30m, fn: first, createEmpty: false) ';
     };
     query += '|> timeShift(duration: 7h, columns: ["_time"]) ';
     query += '|> filter(fn: (r) => r._measurement == "logdays") ';
     query += `|> filter(fn: (r) => r.sn == "${sn}")`;
-    query += '|> filter(fn: (r) => r._field == "temp" or r._field == "humidity" or r._field == "door1" or r._field == "door2" or r._field == "door3")';
-    query += '|> filter(fn: (r) => r.probe == "1" or r.probe == "2")';
-    query += '|> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")';
-    query += '|> keep(columns: ["_time", "temp", "humidity", "door1", "door2", "door3", "probe"])';
+    query += '|> filter(fn: (r) => r._field == "temp" or r._field == "humidity" or r._field == "extMemory" or r._field == "door1" ';
+    query += 'or r._field == "door2" or r._field == "door3" or r._field == "battery" or r._field == "plug") ';
+    query += '|> filter(fn: (r) => r.probe == "1" or r.probe == "2") ';
+    query += '|> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") ';
+    query += '|> keep(columns: ["_time", "temp", "humidity", "extMemory", "door1", "door2", "door3", "probe" , "battery", "plug"]) ';
     const result = await this.influxdb.queryData(query);
     if (result.length > 0) await this.redis.set(`graph:${sn}${filter.split(',').join("")}`, JSON.stringify(result), 1800);
     return result;
