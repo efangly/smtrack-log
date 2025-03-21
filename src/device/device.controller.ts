b9c2294081087062e8eda98baa83b31d5611af14
FangLy <thefangsni@gmail.com>
2025-03-21 10:15:42 +0700

    feat: add history retrieval endpoint to DeviceController and implement query logic in DeviceService

--- src/device/device.controller.ts
+++ src/device/device.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Param, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Query } from '@nestjs/common';
 import { DeviceService } from './device.service';
 import { CreateDeviceDto } from './dto/create-device.dto';
 import { UpdateDeviceDto } from './dto/update-device.dto';
@@ -7,6 +7,11 @@ import { OnlineDto } from '../logday/dto/online.dto';
 @Controller('device')
 export class DeviceController {
   constructor(private readonly deviceService: DeviceService) {}
+
+  @Get('history')
+  async findHistory(@Query('sn') sn: string, @Query('type') type: string, @Query('user') user: string, @Query('filter') filter: string) {
+    return this.deviceService.findHistory(sn, type, user, filter);
+  }
 
   @Post()
   async create(@Body() createDeviceDto: CreateDeviceDto) {
--- src/device/device.service.ts
+++ src/device/device.service.ts
@@ -1,14 +1,19 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateDeviceDto } from './dto/create-device.dto';
 import { UpdateDeviceDto } from './dto/update-device.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { dateFormat } from '../common/utils';
 import { RabbitmqService } from '../rabbitmq/rabbitmq.service';
 import { OnlineDto } from '../logday/dto/online.dto';
+import { InfluxdbService } from '../influxdb/influxdb.service';
 
 @Injectable()
 export class DeviceService {
-  constructor(private readonly prisma: PrismaService, private readonly rabbitmq: RabbitmqService) { }
+  constructor(
+    private readonly prisma: PrismaService, 
+    private readonly rabbitmq: RabbitmqService,
+    private readonly influxdb: InfluxdbService
+  ) {}
 
   async create(createDeviceDto: CreateDeviceDto) {
     return this.prisma.devices.create({
@@ -40,5 +45,24 @@ export class DeviceService {
       where: { serial: id },
       data: updateDeviceDto
     });
+  }
+
+  async findHistory(sn: string, type: string, user: string, filter: string) {
+    if (!sn) throw new BadRequestException('Invalid sn');
+    let query = `from(bucket: "${process.env.INFLUXDB_BUCKET}") `;
+    if (filter) {
+      query += `|> range(start: ${filter}T00:00:00Z, stop: ${filter}T23:59:59Z) `;
+    } else {
+      query += '|> range(start: -1d) ';
+    }
+    query += '|> timeShift(duration: 7h, columns: ["_time"]) ';
+    query += '|> filter(fn: (r) => r._measurement == "history") ';
+    query += `|> filter(fn: (r) => r.service == "${sn}") `;
+    if (type) `|> filter(fn: (r) => r.type == "${type}") `;
+    if (user) `|> filter(fn: (r) => r.user == "${user}") `;
+    query += '|> filter(fn: (r) => r._field == "message") ';
+    query += '|> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") ';
+    query += '|> keep(columns: ["_time", "message"])';
+    return this.influxdb.queryData(query);
   }
 }
