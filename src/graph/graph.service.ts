4f26694aec05965297c27b2b70316517dd592f7a
FangLy <thefangsni@gmail.com>
2025-11-05 12:59:12 +0700

    fix: update image version to 1.1.8 and comment out aggregateWindow in graph service

--- src/graph/graph.service.ts
+++ src/graph/graph.service.ts
@@ -27,7 +27,7 @@ export class GraphService {
         if (!filter.includes(',')) throw new BadRequestException('Invalid filter');
         const date = filter.split(',');
         query += `|> range(start: ${date[0]}, stop: ${date[1]}) `;
-        query += '|> aggregateWindow(every: 30m, fn: first, createEmpty: false) ';
+        // query += '|> aggregateWindow(every: 30m, fn: first, createEmpty: false) ';
     };
     query += '|> timeShift(duration: 7h, columns: ["_time"]) ';
     query += '|> filter(fn: (r) => r._measurement == "logdays") ';
