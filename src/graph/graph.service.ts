a2b09791a82cca0450fc88c401d7989aa9d45975
FangLy <thefangsni@gmail.com>
2026-06-25 13:40:15 +0700

    fix: update image version to 1.1.11a and include frequency parameter in cache key for findAll method

--- src/graph/graph.service.ts
+++ src/graph/graph.service.ts
@@ -8,7 +8,7 @@ export class GraphService {
   async findAll(sn: string, filter: string, freq: string) {
     if (!sn) throw new BadRequestException('Invalid sn');
     if (!filter) throw new BadRequestException('Invalid filter');
-    const cache = await this.redis.get(`graph:${sn}${filter.split(',').join("")}`);
+    const cache = await this.redis.get(`graph:${sn}${filter.split(',').join("")}${freq || ""}`);
     if (cache) return JSON.parse(cache);
     let query = `from(bucket: "${process.env.INFLUXDB_BUCKET}") `;
     switch (filter) {
@@ -38,7 +38,7 @@ export class GraphService {
     query += '|> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") ';
     query += '|> keep(columns: ["_time", "temp", "humidity", "extMemory", "door1", "door2", "door3", "probe" , "battery", "plug", "internet"]) ';
     const result = await this.influxdb.queryData(query);
-    if (result.length > 0) await this.redis.set(`graph:${sn}${filter.split(',').join("")}`, JSON.stringify(result), 1800);
+    if (result.length > 0) await this.redis.set(`graph:${sn}${filter.split(',').join("")}${freq || ""}`, JSON.stringify(result), 1800);
     return result;
   }
 
